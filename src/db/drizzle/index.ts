c3d3fdcdaac6ffc31ee3a0feaa632ff2f3a03b2b
Matheus Pires <matpires_02@outlook.com.br>
2026-04-27 16:28:34 -0300

    Ajuste para publicação

--- src/db/drizzle/index.ts
+++ src/db/drizzle/index.ts
@@ -1,14 +1,50 @@
 import { drizzle } from "drizzle-orm/better-sqlite3";
+import { drizzle as drizzleTurso } from "drizzle-orm/libsql";
 import { postTable } from "./schemas";
 import Database from "better-sqlite3";
 import { resolve } from "path";
+import { createClient } from "@libsql/client";
 
-const sqliteDatabasePath = resolve(process.cwd(), "db.sqlite3");
+const sqliteDatabasePath = resolve(
+  process.cwd(),
+  process.env.DATABASE_URL || "db.sqlite3",
+);
 const sqliteDatabase = new Database(sqliteDatabasePath);
 
-export const drizzleDb = drizzle(sqliteDatabase, {
+/* export const drizzleDb = drizzle(sqliteDatabase, {
   schema: {
     posts: postTable,
   },
   logger: false,
-});
+}); */
+
+const getDrizzleDb = async () => {
+  let db: ReturnType<typeof drizzle | typeof drizzleTurso>;
+
+  if (process.env.NODE_ENV === "production") {
+    // Turso (produção / vercel)
+
+    const client = createClient({
+      url: process.env.DATABASE_URL!,
+      authToken: process.env.DATABASE_TOKEN!,
+    });
+
+    db = drizzleTurso(client, {
+      schema: {
+        posts: postTable,
+      },
+      logger: false,
+    });
+    return db;
+  } else {
+    db = drizzle(sqliteDatabase, {
+      schema: {
+        posts: postTable,
+      },
+      logger: false,
+    });
+    return db;
+  }
+};
+
+export const drizzleDb = await getDrizzleDb();
